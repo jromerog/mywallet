4365f7e621a779969d02df1a729129aa970efa0b
Jorge Romero <jromerog@indra.es>
2014-12-19 13:17:20 +0100

    add funcionality

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,6 +11,8 @@ angular.module('mywalletApp').controller('MainCtrl', [ '$http', '$log' , functio
 
 	var wallet = this;
 	wallet.amounts = [];
+	wallet.newAmount = {};
+	wallet.action = 'entry';
 
 	$http.get('../data/wallet.json')
 		.success(function(data){
@@ -19,4 +21,23 @@ angular.module('mywalletApp').controller('MainCtrl', [ '$http', '$log' , functio
 		.error(function(){
 			$log.log('error');
 		});
+
+	wallet.addAmount = function (){
+		wallet.newAmount.date = Date.now();
+		if(wallet.action === 'entry'){
+			wallet.amounts.push(wallet.newAmount);
+
+			wallet.newAmount = {};
+		}
+
+		// $http.post('../data/wallet.json')
+		// 	.success(function(){
+		// 		$log.log('bien');
+		// 	})
+		// 	.error(function(){
+		// 		$log.log('error');
+		// 	});
+
+
+	};
 }]);
