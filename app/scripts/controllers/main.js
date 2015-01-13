dee87f2cb3d550a2998fecd04a1afac60288d96d
Jorge Romero <jromerog@indra.es>
2015-01-13 14:48:25 +0100

    creating tab selection

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -14,6 +14,16 @@ angular.module('mywalletApp').controller('MainCtrl',['$scope','myStorage', funct
 	var store = 'amounts';
 	myStorage.loadStorage(store);
 
+	$scope.section = 1;
+
+	$scope.selectTab = function(tab){
+		$scope.section = tab;
+	}
+
+	$scope.selectedTab = function(tab){
+		return tab === $scope.section;
+	};
+
 	$scope.Reset = function(){
 		myStorage.resetStorage(store);
 	};
